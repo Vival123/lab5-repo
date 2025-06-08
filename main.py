19b1325960948d36dcc96337cc292eba5a807291
Vival123 <117462617+Vival123@users.noreply.github.com>
2025-06-08 09:13:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def get_valid_csv_file(default="movements.csv"):
         print(f"Ошибка: файл '{path}' должен существовать и иметь расширение .csv!")
 
 def main():
-    print("Changed in original repo")
+    print("Changed in dev (cloned) repo")
     print("=== Лабораторная работа №4. Классы ===")
     
     # Подсчет файлов в директории
